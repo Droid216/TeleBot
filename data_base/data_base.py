9cb1ae347f0220823415531bbf15867af234a9ac
Droid216 <108539215+Droid216@users.noreply.github.com>
2022-11-12 00:14:47 +1000

    Update data_base.py

--- data_base/data_base.py
+++ data_base/data_base.py
@@ -1,7 +1,7 @@
 import sqlite3 as sq
 from create_bot import bot
 
-base = sq.connect('base.db')
+base = sq.connect('./data_base/base.db')
 cur = base.cursor()
 
 
@@ -11,8 +11,7 @@ def sql_start() -> None:
                                                      date_join TEXT, 
                                                      status INT DEFAULT 0, 
                                                      date_status TEXT DEFAULT 0, 
-                                                     money TEXT DEFAULT 0, 
-                                                     stalker INT DEFAULT 0)''')
+                                                     money TEXT DEFAULT 0)''')
     base.execute('''CREATE TABLE IF NOT EXISTS catalog(_id INTEGER PRIMARY KEY AUTOINCREMENT, 
                                                        name TEXT, 
                                                        price TEXT, 
@@ -38,26 +37,6 @@ async def add_photo(state) -> None:
 async def data_user() -> dict:
     dict_users = {}
     for user in cur.execute('SELECT * FROM users').fetchall():
-        dict_users[user[0]] = dict(zip(('name', 'date_join', 'status', 'date_status', 'money', 'stalker'), user[1:]))
+        dict_users[user[0]] = dict(zip(('name', 'date_join', 'status', 'date_status', 'money'), user[1:]))
     base.commit()
     return dict_users
-
-"""
-async def sql_del_product(product_name: str):
-    cur.execute('DELETE FROM catalog WHERE product_name = ?', (product_name, ))
-    base.commit()
-
-
-async def sgl_edit_product(state):
-    async with state.proxy() as data:
-        cur.execute(f'UPDATE catalog SET {data["state"]} = ? WHERE product_name = ?',
-                    (data['new_data'], data['product_name']))
-        base.commit()
-
-
-async def sgl_edit_availability(state, availability: 0 or 1):
-    async with state.proxy() as data:
-        cur.execute('UPDATE catalog SET availability = ? WHERE product_name = ?',
-                    (availability, data['product_name']))
-        base.commit()
-"""
